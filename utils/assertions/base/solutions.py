1537206661ae2961b24bb0cb9935754d18b775c2
Nikita Filonov <63447343+Nikita-Filonov@users.noreply.github.com>
2023-01-08 12:05:54 +0300

    Example report

--- utils/assertions/base/solutions.py
+++ utils/assertions/base/solutions.py
@@ -5,5 +5,5 @@ from utils.assertions.base.expect import expect
 
 def assert_status_code(actual: int, expected: HTTPStatus) -> None:
     expect(expected) \
-        .to_be_equal(actual) \
-        .set_description('Response status code')
+        .set_description('Response status code') \
+        .to_be_equal(actual)
